2195979cd0584f763e4be6a878f2ccef1d3b9d2a
Eric Lam <ehylam.p@gmail.com>
2021-06-26 14:22:55 +1000

    JSON to Shopify liquid code testing

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -8,6 +8,7 @@ const UserInput = props => {
 	return (
 		<div className="user_input">
 			<textarea name="userinput" id="input" cols="30" rows="10" onChange={(e) => props.userInput(e.target.value)}>
+
 			</textarea>
 		</div>
 	 );
--- src/components/UserOutput.jsx
+++ src/components/UserOutput.jsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const handleSections = sections => {
+	const resultArr = [];
+	sections.forEach(( setting ) => {
+		if(setting.type !== 'header' && setting.type !== 'paragraph') {
+			const liquidVar = setting.id[0].toUpperCase() + setting.id.substring(1);
+			const output = {id: `setting.${setting.id}` ,settings: `
+				{% if section.settings.${setting.id} == blank %}
+					{% assign settings${liquidVar} = block.settings.${setting.id} %}
+				{% else %}
+					{% assign settings${liquidVar} = section.settings.${setting.id} %}
+			`}
+			resultArr.push(output);
+		}
+
+	});
+	return resultArr;
+}
+
+const handleBlocks = blocks => {
+
+}
 
 const UserOutput = props => {
+	// const [settings, setSettings] = useState([]);
+	let schema = {};
+	let schemaSections = [];
+	let schemaBlocks = [];
+
+	if(props.output) {
+		try {
+			schema = JSON.parse(props.output)[0];
+			console.log(schema);
+
+			// Sections
+			if(schema.settings.length) {
+				schemaSections = handleSections(schema.settings);
+				console.log(schemaSections);
+			}
+
+			if(schema.blocks.length) {
+				schemaBlocks = handleBlocks(schema.blocks);
+			}
+
+		} catch (e) {
+				return false;
+		}
+
+	}
+
+	const sectionItems = schemaSections.map((section) => {
+		return section.settings;
+	});
+
+
 	return (
 		<div className="user_output">
-			<code>
-				{props.output}
-			</code>
+			<pre id="highlighting" aria-hidden="true">
+				<code>
+					{sectionItems ? sectionItems : 'No Settings'}
+				</code>
+			</pre>
 		</div>
 	 );
 }
